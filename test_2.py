3351f79831b9a8d22b2f27a8ecf54203b485b4d9
zyw0323 <150421469+zyw0323@users.noreply.github.com>
2023-12-22 16:15:22 +0800

    Add files via upload

---
+++ test_2.py
@@ -0,0 +1,22 @@
+import random
+import pytest
+import yaml
+
+@pytest.mark.flaky(reruns=6,reruns_delay=2)
+def test_example():
+    print(3)
+    assert 1==0
+    assert random.choice([True,False])
+
+# def test_simple_assume(x, y):
+#     pytest.assume(x == y)
+#     pytest.assume(True)
+#     pytest.assume(False)
+
+@pytest.mark.run(order=2)
+def test_foo():
+    assert True
+
+@pytest.mark.run(order=1)
+def test_bar():
+    assert True
