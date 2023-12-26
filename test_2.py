e896899b1e5c2a85abbf0bcf6f631a489fbe012b
zyw0323 <150421469+zyw0323@users.noreply.github.com>
2023-12-26 08:58:04 +0800

    test_2.py

--- test_2.py
+++ test_2.py
@@ -5,7 +5,6 @@ import yaml
 @pytest.mark.flaky(reruns=6,reruns_delay=2)
 def test_example():
     print(3)
-    assert 1==0
     assert random.choice([True,False])
 
 # def test_simple_assume(x, y):
